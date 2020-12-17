d3eb73e83b495afc2b523d09d7e7e3b796cc28ab
Phillip Linley <61027312+Solus90@users.noreply.github.com>
2020-12-17 16:10:09 -0500

    Add files via upload
    updated code to properly display images

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 <html>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<form action="/upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
+
+<?php
+$target_dir = "uploads";
+$filenames = preg_grep('/^([^.])/', scandir($target_dir));
+
+foreach ($filenames as $file) {
+    ?>
+
+    <img src="/uploads/<?= $file; ?>">
+
+<?php } ?>
 
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "uploads";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . '/' . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -15,26 +15,22 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
-
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
